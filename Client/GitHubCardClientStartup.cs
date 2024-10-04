c50a09185d19b758ee17cc588130cfc691751172
Ryan Jagdfeld <ryan@jagdfeld.com>
2024-10-04 08:46:36 -0500

    Added client StartUp to register iGitHubCardServices for DI in WebAssembly

---
+++ Client/GitHubCardClientStartup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Oqtane.Services;
+using RyanJagdfeld.Module.GitHubCard.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyanJagdfeld.Module.GitHubCard.Startup
+{
+    public class GitHubCardClientStartup : IClientStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IGitHubCardService, GitHubCardService>();
+        }
+    }
+}
