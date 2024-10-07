1f4abe757b721edb70bb3fe677064e4131030b20
Ryan Jagdfeld <ryan@jagdfeld.com>
2024-10-06 22:30:03 -0500

    Remove old settings

--- Shared/Models/GitHubCard.cs
+++ Shared/Models/GitHubCard.cs
@@ -13,9 +13,6 @@ namespace RyanJagdfeld.Module.GitHubCard.Models
         public int ModuleId { get; set; }
         public string Username { get; set; }
         public string Repo { get; set; }
-        public string Theme { get; set; } = "default";
-        public int Height { get; set; } = 200;
-        public int Width { get; set; } = 400;
         public string CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
         public string ModifiedBy { get; set; }
